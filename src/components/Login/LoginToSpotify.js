ef6af63d36d301d57942d3b9622910c137567783
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-08-30 20:55:46 +0100

    returns names of top artisst! no css yet

--- src/components/Login/LoginToSpotify.js
+++ src/components/Login/LoginToSpotify.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import "./login.css";
 
 const SPACE_DELIMITER = "%20";
@@ -25,35 +24,9 @@ const SCOPES = [
 ];
 const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
 
-const getReturnedParamsFromSpotifyAuth = (hash) => {
-  const stringAfterHashtag = hash.substring(1);
-  const paramsInUrl = stringAfterHashtag.split("&");
-  const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
-    console.log(currentValue);
-    const [key, value] = currentValue.split("=");
-    accumulater[key] = value;
-    return accumulater;
-  }, {});
-
-  return paramsSplitUp;
-};
-
 const LoginToSpotify = () => {
-  useEffect(() => {
-    if (window.location.hash) {
-      const { access_token, expires_in, token_type } =
-        getReturnedParamsFromSpotifyAuth(window.location.hash);
-
-      localStorage.clear();
-
-      localStorage.setItem("AT", access_token);
-      localStorage.setItem("tokenType", token_type);
-      localStorage.setItem("expiresIn", expires_in);
-    }
-  });
-
   const handleLogin = () => {
-    window.location = `${process.env.REACT_APP_AUTHORIZE_URL}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}&scopes=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+    window.location = `${process.env.REACT_APP_AUTHORIZE_URL}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true&state=code`;
   };
 
   return (
--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -3,36 +3,59 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Poster = () => {
-  const token = localStorage.getItem("AT");
+  //const token = localStorage.getItem("AT");
+  const [token, setToken] = useState("");
+  const tokenType = localStorage.getItem("tokenType");
   const [data, setData] = useState({});
 
   //const FETCH_URL = `https://api.spotify.com/v1/me/top/artists?time_range=${len}_term&limit=10&offset=5`
 
-  // useEffect(() => {
+  const getReturnedParamsFromSpotifyAuth = (hash) => {
+    const stringAfterHashtag = hash.substring(1);
+    const paramsInUrl = stringAfterHashtag.split("&");
+    const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
+      console.log(currentValue);
+      const [key, value] = currentValue.split("=");
+      accumulater[key] = value;
+      return accumulater;
+    }, {});
 
-  //   // if (localStorage.getItem("AT")) {
-  //   //   setToken(localStorage.getItem("AT"));
-  //   // }
-  //   // console.log(token);
-  // }, []);
+    return paramsSplitUp;
+  };
 
-  console.log(token);
+  useEffect(() => {
+    // getReturnedParamsFromSpotifyAuth(window.location.hash);
+    if (window.location.hash) {
+      const { access_token, expires_in, token_type } =
+        getReturnedParamsFromSpotifyAuth(window.location.hash);
+      localStorage.clear(); // tried localStorage.removeItem("AT")
+      localStorage.setItem("AT", access_token);
+      localStorage.setItem("tokenType", token_type);
+      localStorage.setItem("expiresIn", expires_in);
+    }
+  }, []);
+  useEffect(() => {
+    if (localStorage.getItem("AT")) {
+      setToken(localStorage.getItem("AT"));
+    }
+    console.log(token);
+  }, []);
+
+  console.log(token); // works, it IS in local storage
+  console.log(tokenType);
 
   // "GET" "https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=1&offset=0"
   // -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQCGkqmbB6B8NtItE0mSsH6Ye2-jFX2NMftCvkNRrcRiJlarLb6XlRycuNN0FTzhn7NXg7sPXBYlkfpyFmpoqFxVLCbGA9o_R082zltFMIUGlarRToSqYJJbekpu6dcuzsdfqf1oYd5szJi83GrqaQ"
 
   const getArtists = (len = "long") => {
-    console.log(token)
+    console.log(token); // works
+    // network gives error: no token provided
     axios
-      .get(
-        `https://api.spotify.com/v1/me/top/artists?time_range=${len}_term&limit=10&offset=5`,
-        {
-          headers: {
-            Authorization: "Bearer " + token,
-           
-          },
-        }
-      )
+      .get(`https://api.spotify.com/v1/me/top/artists`, {
+        headers: {
+          Authorization: `${tokenType} ${token}`,
+        },
+      })
       .then((response) => {
         setData(response.data);
       })
