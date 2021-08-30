ed681bf7f22dbfb25fb69b285e7e537849124623
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-08-30 21:59:09 +0100

    all functionality

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Poster = () => {
-  //const token = localStorage.getItem("AT");
   const [token, setToken] = useState("");
   const tokenType = localStorage.getItem("tokenType");
   const [data, setData] = useState({});
@@ -24,40 +23,31 @@ const Poster = () => {
   };
 
   useEffect(() => {
-    // getReturnedParamsFromSpotifyAuth(window.location.hash);
     if (window.location.hash) {
       const { access_token, expires_in, token_type } =
         getReturnedParamsFromSpotifyAuth(window.location.hash);
-      localStorage.clear(); // tried localStorage.removeItem("AT")
+      localStorage.clear();
       localStorage.setItem("AT", access_token);
       localStorage.setItem("tokenType", token_type);
       localStorage.setItem("expiresIn", expires_in);
     }
-  }, []);
-  useEffect(() => {
     if (localStorage.getItem("AT")) {
       setToken(localStorage.getItem("AT"));
     }
-    console.log(token);
-  }, []);
-
-  console.log(token); // works, it IS in local storage
-  console.log(tokenType);
-
-  // "GET" "https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=1&offset=0"
-  // -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQCGkqmbB6B8NtItE0mSsH6Ye2-jFX2NMftCvkNRrcRiJlarLb6XlRycuNN0FTzhn7NXg7sPXBYlkfpyFmpoqFxVLCbGA9o_R082zltFMIUGlarRToSqYJJbekpu6dcuzsdfqf1oYd5szJi83GrqaQ"
+    getArtists();
+  }, [token]);
 
   const getArtists = (len = "long") => {
-    console.log(token); // works
-    // network gives error: no token provided
     axios
-      .get(`https://api.spotify.com/v1/me/top/artists`, {
+      .get(`https://api.spotify.com/v1/me/top/artists?time_range=${len}_term`, {
         headers: {
           Authorization: `${tokenType} ${token}`,
         },
       })
       .then((response) => {
+        console.log("response", response);
         setData(response.data);
+        console.log("response.data", response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -66,19 +56,22 @@ const Poster = () => {
 
   return (
     <div>
+      <button onClick={() => getArtists("medium")} className="show1">
+        6 months
+      </button>
+      <button onClick={() => getArtists("short")} className="show2">
+        4 weeks
+      </button>
+      <button onClick={() => getArtists()} className="show3">
+        reset
+      </button>
       <h1>MYFEST</h1>
-      <button onClick={() => getArtists()} className="show1">
-        create
-      </button>{" "}
-      {data?.items ? data.items.map((item) => <p>{item.name}</p>) : null}
-      {/* <button onClick={() => getArtists("short")} className="show2">
-        show last 4 weeks
-      </button> */}
+
       <div className="posterBox">
         <ul>
-          {/* {data?.items
+          {data?.items
             ? data.items.map((art, index) => <li key={index}>{art.name}</li>)
-            : null} */}
+            : null}
         </ul>
       </div>
     </div>
