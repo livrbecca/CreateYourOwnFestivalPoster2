118a8b5985dfb6d1490605dc5a15332c3841b628
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-09-01 17:07:39 +0100

    reduced user scopes

--- src/components/Login/LoginToSpotify.js
+++ src/components/Login/LoginToSpotify.js
@@ -2,25 +2,7 @@ import "./login.css";
 
 const SPACE_DELIMITER = "%20";
 const SCOPES = [
-  "ugc-image-upload",
-  "user-read-playback-state",
-  "user-modify-playback-state",
-  "user-read-currently-playing",
-  "streaming",
-  "app-remote-control",
-  "user-read-email",
-  "user-read-private",
-  "playlist-read-collaborative",
-  "playlist-modify-public",
-  "playlist-read-private",
-  "playlist-modify-private",
-  "user-library-modify",
-  "user-library-read",
   "user-top-read",
-  "user-read-playback-position",
-  "user-read-recently-played",
-  "user-follow-read",
-  "user-follow-modify",
 ];
 const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
 
