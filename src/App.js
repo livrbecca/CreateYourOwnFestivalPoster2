97f93d95c4dfe09a7e13e87398952ad2227e57dc
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-09-02 03:58:58 +0100

    removed playlist

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import LoginToSpotify from "./components/Login/LoginToSpotify";
 import Poster from "./components/Poster/Poster";
-import CreatePlaylist from "./components/Playlist/CreatePlaylist";
+
 
 function App() {
   return (
@@ -9,7 +9,6 @@ function App() {
       <Switch>
         <Route exact path="/" render={() => <LoginToSpotify />} />
         <Route exact path="/yourposter" render={() => <Poster />} />
-        <Route exact path="/createplaylist" render={() => <CreatePlaylist />} />
       </Switch>
     </BrowserRouter>
   );
